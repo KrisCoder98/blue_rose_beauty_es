2f75a34380be05e612606036126b94e01171ad69
Kris <kristian.scarpellino@gmail.com>
2026-04-22 08:29:49 +0200

    HomePage statica

---
+++ app/customObject/User.ts
@@ -0,0 +1,19 @@
+"use client"
+
+export type Gender = "M" | "F" | "U";
+export type ProfileType = "Admin" | "User";
+
+type UserId = `US-${number}`;
+type ImagePath = `/${string}.${"png" | "jpg" | "jpeg" | "webp"}`;
+
+export type User = {
+    id: UserId;
+    avatar: ImagePath;
+    gender: Gender;
+    name: string;
+    profile: ProfileType;
+    preferences: {
+        theme?: "dark" | "light";
+        notifications?: boolean;
+    };
+}
